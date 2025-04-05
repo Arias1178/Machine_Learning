f7e72474eb32227458f486d650b276976a73893b
Arias1178 <santiagoarias1178@gmail.com>
2025-04-05 14:07:49 -0500

    regresion

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ import regrecionlineal
 
 app = Flask(__name__)
 
+#Salida Basica
 @app.route("/")
 def home():
     return "Hello, Bebes"
 
+#Ejemplo clase
 @app.route("/hello/<name>")
 def hello_there(name):
     now = datetime.now()
@@ -18,29 +20,34 @@ def hello_there(name):
     if match_object:
         clean_name = match_object.group(0)
     else:
-        clean_name = "Friend"
+        clean_name = "Friends"
 
     return render_template("index.html", name=clean_name, formatted_now=formatted_now)
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-
+#Pagina casos de uso
 @app.route("/exampleHTML/")
 def exampleHTML():
-    return render_template("CasosDeUso.html")
+    return render_template("casosdeuso.html")
 
+#Mapa conceptual
+@app.route("/mapa/")
+def mapaHTML():
+    return render_template("mapaconceptual.html")
 
+#Rgresion Lineal
 @app.route("/regrecionlineal", methods=["GET","POST"])
 
 def regrecionlineal_endpoint():
     calcularResultado = None
     grafico_url = None
     if request.method=="POST":
-       horas=float(request.form["horas"])
+       horas=float(request.form["Tiempo_actividad"])
        print(f"Valor recibido: {horas}")
-       calcularResultado = regrecionlineal.calculateGrade (horas)
-       grafico_url = regrecionlineal.grafica_regresion(nueva_hora=horas)
+       calcularResultado = regrecionlineal.calculateBienestar(horas)
+       grafico_url = regrecionlineal.grafica_regresion(nuevo_tiempo=horas)
     return render_template("regrecionlineal.html", resultado=calcularResultado, grafico_url=grafico_url)
 
 if __name__ == "__main__":
