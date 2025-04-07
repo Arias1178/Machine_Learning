f5952f83badd91b6ba56f45d3fd149af329a29fd
kipvargas <perezvargaskarlaisabel@gmail.com>
2025-04-07 13:48:24 -0600

    Modelos de clasificacion primer intento

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import re
 from datetime import datetime
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 import regrecionlineal
+import pyodbc
 
 app = Flask(__name__)
 
+# Configuración de conexión
+server = 'ModelosClasificacion.mssql.somee.com'
+database = 'ModelosClasificacion'
+username = 'isaVargas_SQLLogin_1'
+password = 'djz3g2kkhk'
+driver = '{ODBC Driver 17 for SQL Server}'
+connection_string = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+
+#Salida Basica
 @app.route("/")
 def home():
     return "Hello, Bebes"
@@ -22,17 +33,13 @@ def hello_there(name):
 
     return render_template("index.html", name=clean_name, formatted_now=formatted_now)
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
+#Pagina casos de uso
 @app.route("/exampleHTML/")
 def exampleHTML():
     return render_template("casosdeuso.html")
 
 
 @app.route("/regrecionlineal", methods=["GET","POST"])
-
 def regrecionlineal_endpoint():
     calcularResultado = None
     grafico_url = None
@@ -42,6 +49,174 @@ def regrecionlineal_endpoint():
        calcularResultado = regrecionlineal.calculateGrade (horas)
        grafico_url = regrecionlineal.grafica_regresion(nueva_hora=horas)
     return render_template("regrecionlineal.html", resultado=calcularResultado, grafico_url=grafico_url)
+
+#Info Regresion Logistica
+@app.route('/infoRegresionLogistica/')
+def infoRegresionLogistica():
+    try:
+        conn = pyodbc.connect(connection_string)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM modelo where idModelo = 1;")
+        row = cursor.fetchone()
+        if row:
+            modelo = {
+                'nombre': row.nombre,
+                'descripcion': row.descripcion,
+                'enlace': row.enlace,
+                'imagen': row.imagen
+            }
+            return render_template('infoRegresionLogistica.html', modelo=modelo)
+        else:
+            return "No se encontró ningún modelo en la base de datos."
+    except Exception as e:
+        return f"Error: {e}"
+
+#Info KNN
+@app.route('/infoKNN/')
+def infoKNN():
+    try:
+        conn = pyodbc.connect(connection_string)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM modelo where idModelo = 2;")
+        row = cursor.fetchone()
+        if row:
+            modelo = {
+                'nombre': row.nombre,
+                'descripcion': row.descripcion,
+                'enlace': row.enlace,
+                'imagen': row.imagen
+            }
+            return render_template('infoKNN.html', modelo=modelo)
+        else:
+            return "No se encontró ningún modelo en la base de datos."
+    except Exception as e:
+        return f"Error: {e}"
+
+#Info Arboles Decision
+@app.route('/infoArbolesDecision/')
+def infoArbolesDecision():
+    try:
+        conn = pyodbc.connect(connection_string)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM modelo where idModelo = 3;")
+        row = cursor.fetchone()
+        if row:
+            modelo = {
+                'nombre': row.nombre,
+                'descripcion': row.descripcion,
+                'enlace': row.enlace,
+                'imagen': row.imagen
+            }
+            return render_template('infoArbolesDecision.html', modelo=modelo)
+        else:
+            return "No se encontró ningún modelo en la base de datos."
+    except Exception as e:
+        return f"Error: {e}"
+
+#Info Random Forest
+@app.route('/infoRandomForest/')
+def infoRandomForest():
+    try:
+        conn = pyodbc.connect(connection_string)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM modelo where idModelo = 4;")
+        row = cursor.fetchone()
+        if row:
+            modelo = {
+                'nombre': row.nombre,
+                'descripcion': row.descripcion,
+                'enlace': row.enlace,
+                'imagen': row.imagen
+            }
+            return render_template('infoRandomForest.html', modelo=modelo)
+        else:
+            return "No se encontró ningún modelo en la base de datos."
+    except Exception as e:
+        return f"Error: {e}"
+
+#Info SVM
+@app.route('/infoSVM/')
+def infoSVM():
+    try:
+        conn = pyodbc.connect(connection_string)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM modelo where idModelo = 5;")
+        row = cursor.fetchone()
+        if row:
+            modelo = {
+                'nombre': row.nombre,
+                'descripcion': row.descripcion,
+                'enlace': row.enlace,
+                'imagen': row.imagen
+            }
+            return render_template('infoSVM.html', modelo=modelo)
+        else:
+            return "No se encontró ningún modelo en la base de datos."
+    except Exception as e:
+        return f"Error: {e}"
+
+#Info XGBoost
+@app.route('/infoXGBoost/')
+def infoXGBoost():
+    try:
+        conn = pyodbc.connect(connection_string)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM modelo where idModelo = 6;")
+        row = cursor.fetchone()
+        if row:
+            modelo = {
+                'nombre': row.nombre,
+                'descripcion': row.descripcion,
+                'enlace': row.enlace,
+                'imagen': row.imagen
+            }
+            return render_template('infoXGBoost.html', modelo=modelo)
+        else:
+            return "No se encontró ningún modelo en la base de datos."
+    except Exception as e:
+        return f"Error: {e}"
+
+#Info AdaBoost
+@app.route('/infoAdaBoost/')
+def infoAdaBoost ():
+    try:
+        conn = pyodbc.connect(connection_string)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM modelo where idModelo = 7;")
+        row = cursor.fetchone()
+        if row:
+            modelo = {
+                'nombre': row.nombre,
+                'descripcion': row.descripcion,
+                'enlace': row.enlace,
+                'imagen': row.imagen
+            }
+            return render_template('infoAdaBoost.html', modelo=modelo)
+        else:
+            return "No se encontró ningún modelo en la base de datos."
+    except Exception as e:
+        return f"Error: {e}"
+
+#Info Naive Bayes
+@app.route('/infoNaiveBayes/')
+def infoNaiveBayes ():
+    try:
+        conn = pyodbc.connect(connection_string)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM modelo where idModelo = 8;")
+        row = cursor.fetchone()
+        if row:
+            modelo = {
+                'nombre': row.nombre,
+                'descripcion': row.descripcion,
+                'enlace': row.enlace,
+                'imagen': row.imagen
+            }
+            return render_template('infoNaiveBayes.html', modelo=modelo)
+        else:
+            return "No se encontró ningún modelo en la base de datos."
+    except Exception as e:
+        return f"Error: {e}"
 
 if __name__ == "__main__":
     app.run(debug=True)#ejecutamo la aplicacion sin importar los errores del .py
