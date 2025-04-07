aee52d06776dd25c02e0d2e20fec21cfdc4e4abc
Arias1178 <santiagoarias1178@gmail.com>
2025-04-07 15:38:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ import regrecionlineal
 
 app = Flask(__name__)
 
+#Salida Basica
 @app.route("/")
 def home():
     return "Hello, Bebes"
 
+#Ejemplo clase
 @app.route("/hello/<name>")
 def hello_there(name):
     now = datetime.now()
@@ -18,50 +20,35 @@ def hello_there(name):
     if match_object:
         clean_name = match_object.group(0)
     else:
-        clean_name = "Friend"
+        clean_name = "Friends"
 
     return render_template("index.html", name=clean_name, formatted_now=formatted_now)
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-
+#Pagina casos de uso
 @app.route("/exampleHTML/")
 def exampleHTML():
-    return render_template("example.html")
+    return render_template("casosdeuso.html")
 
-
-<<<<<<< Updated upstream
-=======
-
-#Menu
-@app.route("/menuformulario", methods=["GET", "POST"])
-def menu_formulario():
-    seccion = None
-    if request.method == "POST":
-        seccion = request.form.get("seccion")
-    return render_template("menu.html", seccion=seccion)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+#Mapa conceptual
+@app.route("/mapa/")
+def mapaHTML():
+    return render_template("mapaconceptual.html")
 
 #Rgresion Lineal
->>>>>>> Stashed changes
 @app.route("/regrecionlineal", methods=["GET","POST"])
+
 def regrecionlineal_endpoint():
     calcularResultado = None
     grafico_url = None
     if request.method=="POST":
-       horas=float(request.form["horas"])
+       horas=float(request.form["Tiempo_actividad"])
        print(f"Valor recibido: {horas}")
-       calcularResultado = regrecionlineal.calculateGrade (horas)
-       grafico_url = regrecionlineal.grafica_regresion(nueva_hora=horas)
+       calcularResultado = regrecionlineal.calculateBienestar(horas)
+       grafico_url = regrecionlineal.grafica_regresion(nuevo_tiempo=horas)
     return render_template("regrecionlineal.html", resultado=calcularResultado, grafico_url=grafico_url)
 
 if __name__ == "__main__":
     app.run(debug=True)#ejecutamo la aplicacion sin importar los errores del .py
-
-#Regresion Logistica
-@app.route("/RegresionLogistica/")
-def regresion_logistica():
-    return render_template("menu_regresion_logistica.html")
